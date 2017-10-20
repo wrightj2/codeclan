83809ee814946181cdf45410d2f3be5a2e62c9d3
Jon Wright <github@jonathanwright.net>
2017-10-20 13:12:28 +0100

    Add files via upload

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,29 @@
+
+
+ def add(first_number,second_number)
+   return first_number + second_number
+ end
+
+ def subtract(first_number,second_number)
+   return first_number - second_number
+ end
+
+ def multiply(first_number,second_number)
+   return first_number * second_number
+ end
+
+ def divide(first_number,second_number)
+   return first_number / second_number
+ end
+
+ def length_of_string(first_string)
+   return first_string.length
+ end
+
+ def join_string(first_string,second_string)
+   return first_string + second_string
+ end
+
+ def add_string_as_number(first_string,second_string)
+   return first_string.to_i() + second_string.to_i()
+ end
---
+++ ruby_functions_spec.rb
@@ -0,0 +1,44 @@
+require( 'minitest/autorun' )
+require_relative( './ruby_functions_practice' )
+
+class FunctionsPracticeTest < MiniTest::Test
+
+  def test_add()
+    add_result = add( 1, 2 )
+    assert_equal( 3, add_result )
+  end
+
+  def test_subtract()
+    subtract_result = subtract( 10, 5 )
+    assert_equal( 5, subtract_result )
+  end
+
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
+
+  def test_divide()
+    divide_result = divide( 10, 2 )
+    assert_equal( 5, divide_result )
+  end
+
+  def test_length_of_string()
+    test_string = "A string of length 21"
+    length_of_string = length_of_string( test_string )
+    assert_equal( 21, length_of_string )
+  end
+
+  def test_join_string()
+    string_1 = "Mary had a little lamb, "
+    string_2 = "it's fleece was white as snow"
+    joined_string = join_string( string_1, string_2 )
+    assert_equal( "Mary had a little lamb, it's fleece was white as snow", joined_string )
+  end
+
+  def test_add_string_as_number()
+    add_result = add_string_as_number( "1", "2" )
+    assert_equal( 3, add_result )
+  end
+
+end
